be199c94a72f5df2a56f556c56877cf62126bbd6
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-08 23:39:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   case array
     when array.length <= 1 
       array.to_s
-    when array.length <= 2
+    when array.length = 2
       array.join(" and ")
     else
       array[0..-2].join(", ") + ", and " + array[-1]
