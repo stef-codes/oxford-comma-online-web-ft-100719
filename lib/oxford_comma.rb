0900e501e00f0612f805069d6c84172fcc82481d
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-08 23:35:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   case array
-    when array.length > 2
+    when array.length <= 2
+      array.join(" and ")
+    else
       array[0..-2].join(", ") + " and " + array[-1]
     else array.join(" and ")
   end
