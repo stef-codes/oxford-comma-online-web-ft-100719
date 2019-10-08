335b6595fa53828175397105e62cc2be8994c0f5
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-08 23:59:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
-    if array.length <= 1
-      array.join("") 
-    elsif array = 2 
-      array.join("and")
-    else
-      array[0..-2].join(", ") + ", and " + array[-1]
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
   end
 end 
