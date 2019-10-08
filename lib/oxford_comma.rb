6eef81d3a5ec6e9630ac73d7dc9302a4bd67592f
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-08 23:44:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,3 @@
 def oxford_comma(array)
-  case array 
-    when array.length > 2
-      array[0..-2].join(", ") + ", and " + array[-1]
-    when array.length <= 2
-      array.join(" and ")
-    else
-      array.to_s
-  end
-end
+  
+end 
