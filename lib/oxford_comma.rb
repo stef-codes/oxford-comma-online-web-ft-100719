070488c4ec7ee27d1e72acd63239cdcbff8a6cc4
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-08 23:50:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
-    return array.join("") if array.length <= 1
-    array[0..-2].join(", ") + ", and " + array[-1]
-    else array.join("and")
+    if array.length <= 1
+      return array.join("") 
+    elsif array = 2 
+      array.join("and")
+    else
+      array[0..-2].join(", ") + ", and " + array[-1]
   end
 end 
